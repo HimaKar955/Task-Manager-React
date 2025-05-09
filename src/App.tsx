f2f85d8beca08c8c089088d36873c4ca4deff869
Himakar805 <himakar805@gmail.com>
2025-05-09 23:58:47 +0530

    fix: pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const App: React.FC = () => {
-  return (
-    <div>
-      Application running
-    </div>
-  );
-};
+  return <div>Application running</div>
+}
 
-export default App;
+export default App
